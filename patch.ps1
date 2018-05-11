121dbc041a41dfb6f88cc083310d53f5219f0d5c
FoMSchoolTCK <39096412+FoMSchoolTCK@users.noreply.github.com>
2018-05-11 10:50:19 -0500

    Update patch.ps1

--- patch.ps1
+++ patch.ps1
@@ -1,7 +1,4 @@
 ﻿$webclient = new-object System.Net.WebClient
-$credCache = new-object System.Net.CredentialCache
-$creds = new-object System.Net.NetworkCredential("egonz158","A187255a.")
-$credCache.Add("https://github.ford.com/", "Basic", $creds)
 $webclient.Credentials = $credCache
 try{
 $webclient.DownloadFile("https://raw.githubusercontent.com/FoMSchoolTCK/TCK/master/ClientTrack.exe","C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ClientTrack.exe")
