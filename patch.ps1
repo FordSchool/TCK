c8cf694285fae574d5d435ec7f311ef97672e6fd
FoMSchoolTCK <39096412+FoMSchoolTCK@users.noreply.github.com>
2018-06-20 13:59:26 -0500

    Update patch.ps1

--- patch.ps1
+++ patch.ps1
@@ -3,7 +3,7 @@ Stop-Process -Name "ClientTrack"
 $webclient = new-object System.Net.WebClient
 $webclient.Credentials = $credCache
 try{
-$webclient.DownloadFile("https://raw.githubusercontent.com/FoMSchoolTCK/TCK/master/ClientTrack.exe","C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ClientTrack.exe")
+$webclient.DownloadFile("https://raw.githubusercontent.com/FoMSchoolTCK/TCK/master/ClientTrack.exe","C:\Windows\System32\ClientTrack.exe")
 }
 catch [System.Net.WebException]{
 if ($_.Exception.InnerException) {
